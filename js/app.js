cf3bcde4e9dfdd589d9ad1b36c22eb2e7a033f9d
Jordan Mor <jordanmor@gmail.com>
2018-07-26 11:29:00 -0400

    Add pagination buttons and event listener

--- js/app.js
+++ js/app.js
@@ -4,11 +4,13 @@ $('.page').append('<div class="pagination"><ul></ul></div>');
                       CACHED VARIABLES
 ===============-=============-=============-===========*/
 const $studentList = $('.student-item');
+const $paginationUl = $('.pagination ul');
 
 /*=============-=============-=============-=============
                         FUNCTIONS
 ===============-=============-=============-===========*/
 
+// split a copy of the $studentList into an array of paginated students
 function paginate($list) {
   // copy jQuery collection to avoid manipulating the original list of students
   const arrCopy = [...$list]; // use spread operator to make copy 
@@ -20,10 +22,38 @@ function paginate($list) {
   return newArr;
 }
 
-function showPage($list, pageNum = 1) { // pageNum parameter has default value of 1;
-  const pages = paginate($list);
-  $list.hide();
-  $(pages[pageNum - 1]).show(); // pageNum - 1 to match index numbers in pages array
+function createPaginationButtons($list, pages, currentPage) {
+  let buttons = '';
+  // only create buttons if there is more than one page of students
+  if(pages.length > 1) {
+    for(let i = 1; i <= pages.length; i++) {
+        if (i === currentPage) { // the current page is given the class 'active'
+            buttons += `<li><a class="active" href="#">${i}</a></li>`;
+        } else {
+            buttons += `<li><a href="#">${i}</a></li>`;
+        }
+    }
+  }
+  $paginationUl.html(buttons);
+
+  // Event Listener
+  $('.pagination li').on('click', event => {
+    // when button is clicked, it's parsed text number determines the displayed current page
+    const currentPage = parseInt(event.target.textContent);
+    showPage($list, currentPage);
+  });
 }
+
+// function displays the current paginated page
+function showPage($list, currentPage = 1) { // currentPage parameter has default value of 1;
+  const pages = paginate($list);
+  createPaginationButtons($list, pages, currentPage);
+  $list.hide();
+  $(pages[currentPage - 1]).show(); // currentPage - 1 to match index numbers in pages array
+}
+
+/*=============-=============-=============-=============
+                      PROGRAM INIT
+===============-=============-=============-===========*/
 
 showPage($studentList);
