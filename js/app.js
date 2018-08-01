23de4e4eaaccde8a6b6886a532043b98c15c5324
Jordan Mor <jordanmor@gmail.com>
2018-08-01 06:49:53 -0400

    Improve comments

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-// Unobtrusive JavaScript used to append HTML for pagination and search bar
+// Unobtrusive JavaScript used to append html for pagination and search bar
 $('.page').append('<div class="pagination"><ul></ul></div>');
 $('.page-header').append(`<div class="student-search">
                             <input type="text">
@@ -14,28 +14,33 @@ const $searchInput = $('.student-search input');
 const $searchButton = $('#searchButton');
 
 let filtered = false;
+
 /*=============-=============-=============-=============
                         FUNCTIONS
 ===============-=============-=============-===========*/
 
-// split a copy of the student list into an array of paginated students
+// ---------- Paginate List ---------- //
+
+// Split a copy of the student list into an array of paginated students
 function paginate(list) {
-  // copy jQuery collection to avoid manipulating the original list of students
-  const arrCopy = [...list]; // use spread operator to make copy 
+  // Copy jQuery collection to avoid manipulating the original list of students
+  const arrCopy = [...list]; // Use spread operator to make copy 
   const newArr = [];
-  // using splice method, while loop will end when arrCopy is empty
+  // Use splice method. While loop will end when arrCopy is empty
   while(arrCopy.length) {
     newArr.push(arrCopy.splice(0, 10));
   }
   return newArr;
 }
 
+// ---------- Create Pagination Buttons ---------- //
+
 function createPaginationButtons(list, pages, currentPage) {
   let buttons = '';
-  // only create buttons if there is more than one page of students
+  // Only create buttons if there is more than one page of students
   if(pages.length > 1) {
     for(let i = 1; i <= pages.length; i++) {
-      if (i === currentPage) { // the current page is given the class 'active'
+      if (i === currentPage) { // Current page is given the class 'active'
         buttons += `<li><a class="active" href="#">${i}</a></li>`;
       } else {
         buttons += `<li><a href="#">${i}</a></li>`;
@@ -44,17 +49,18 @@ function createPaginationButtons(list, pages, currentPage) {
   }
   $paginationUl.html(buttons);
 
-  // the filtered variable helps determine if the show all button is displayed
+  // The filtered variable helps determine if the show all button is displayed
   if (filtered) {
     $paginationUl.append(`<li><a class="show-all">Show All</a></li>`);
   }
-  // Event Listener
+
+  // --- Event Listeners --- //
   $('.pagination li').on('click', event => {
-    // when button is clicked, it's parsed text number determines the displayed current page
+    // When button is clicked, it's parsed text number determines the displayed current page
     const currentPage = parseInt(event.target.textContent);
     showPage(list, currentPage);
   });
-  // clicking show all button clears input and resets page to show unfiltered paginated student list
+  // Clicking show all button clears input and resets page to show unfiltered paginated student list
   $('.show-all').on('click', () => {
     $searchInput.val('');
     filtered = false;
@@ -62,9 +68,11 @@ function createPaginationButtons(list, pages, currentPage) {
   });
 }
 
-// function displays the current paginated page
-function showPage(list, currentPage = 1) { // currentPage parameter has default value of 1;
-  // display html message if there are no students in list
+// ---------- Show Pages ---------- //
+
+// Function displays the current paginated page
+function showPage(list, currentPage = 1) { // CurrentPage parameter has default value of 1;
+  // Display html message if there are no students in list
   if(!list.length) {
     $('.page-header h2').text('No Students Found');
   } else {
@@ -74,16 +82,18 @@ function showPage(list, currentPage = 1) { // currentPage parameter has default 
   const pages = paginate(list);
   createPaginationButtons(list, pages, currentPage);
   $studentList.hide();
-  $(pages[currentPage - 1]).show(); // currentPage - 1 to match index numbers in pages array
+  $(pages[currentPage - 1]).show(); // CurrentPage - 1 to match index numbers in pages array
 }
+
+// ---------- Filter List ---------- //
 
 function filterList(event) {
   const value = $searchInput.val().toLowerCase().trim();
-  // input value is only emptied when event target is the clicked search button
+  // Input value is only emptied when event target is the clicked search button
   if(event.target.tagName === 'BUTTON') {
     $searchInput.val('');
   }
-  // student included in filtered array if input value found in that student's name or email
+  // Student included in filtered array if input value found in that student's name or email
   const $filteredList = $studentList.filter(function() {
     const name = $(this).find('h3').text();
     const email = $(this).find('.email').text();
@@ -103,7 +113,7 @@ showPage($studentList);
 /*=============-=============-=============-=============
                     EVENT LISTENERS
 ===============-=============-=============-===========*/
-// filterList() is fired every time a letter is entered into the search input
+// Function filterList is fired every time a letter is entered into the search input
 $searchInput.on('keyup', filterList);
 
 $searchButton.on('click', filterList);
